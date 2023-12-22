eb67888e40f46a2992f037cdb4dfba93dd707976
frankharerimana <fharerimana@objectivity.co.uk>
2023-12-22 14:59:48 +0200

    configures the main route

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import usersRoutes from './users';
+import authRoutes from './auth';
+import authMiddleware from '../middlewares/auth.middleware';
+
+const routes = Router();
+
+const entryPoint = Router();
+entryPoint.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'welcome to user management',
+  });
+});
+
+routes.use(entryPoint, authRoutes);
+
+routes.use(authMiddleware.isAuthenticated, usersRoutes)
+
+export default routes;
